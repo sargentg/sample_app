e7576e157df49ea5691612387247ff835bf29556
George Sargent <sargentg@uww.edu>
2011-10-05 22:23:52 -0500

    Chapter 4 finished

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,9 +12,10 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title", 
-          :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
+  
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
@@ -24,7 +25,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title", 
-          :content => "| Contact")
+                                    :content => "| Contact")
     end
   end
   
@@ -37,8 +38,20 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-          :content => "| About")
+                                    :content => "| About")
     end
-    
+  end
+  
+  describe "GET help" do
+      it "should be successful" do
+        get 'help'
+        response.should be_success
+      end
+      
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => "| Help")
+    end
   end
 end
